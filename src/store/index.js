aca29dfffe54c6c568c3010a319ce78c906e9c36
Igor Gruzdo <igruzdo@yandex.ru>
2022-10-23 13:33:59 +0300

    added middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,22 @@
-import { combineReducers, createStore, compose } from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import heroesReducer from '../reducers/heroes';
 import filtersReducer from '../reducers/filters';
+
+//либо вместо store можно записать ({dispatch, getState})
+// const stringMiddleware = ({dispatch, getState}) => {
+const stringMiddleware = (store) => {
+// next это тот же dispatch, который обозначает функцию, которая вызовется в следующем миддлваре
+    return (next) => {
+        return (action) => {
+            if (typeof action === 'string') {
+                return next({
+                    type: action
+                })
+            }
+            return next(action)
+        }
+    }
+}
 
 const enhanser = (createStore) => (...args) => {
     const store = createStore(...args)
@@ -18,8 +34,11 @@ const enhanser = (createStore) => (...args) => {
 }
 
 const store = createStore(
-    combineReducers({heroesReducer,filtersReducer}), 
-    compose(enhanser, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-    );
+    combineReducers({heroesReducer,filtersReducer}),
+    compose(
+        applyMiddleware(stringMiddleware),
+        compose(enhanser, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+    ),
+);
 
 export default store;
